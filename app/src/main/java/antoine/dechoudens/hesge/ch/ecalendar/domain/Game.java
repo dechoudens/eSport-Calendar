29fa9c5460bff39851955b18e3fc25fba480bf58
Antoine de Choudens <antoine.dechoudens@etu.hesge.ch>
2017-05-26 11:51:11 +0200

    faut trouver comment passer la permission

---
+++ app/src/main/java/antoine/dechoudens/hesge/ch/ecalendar/domain/Game.java
@@ -0,0 +1,21 @@
+package antoine.dechoudens.hesge.ch.ecalendar.domain;
+
+/**
+ * Created by Meckanik on 26.05.2017.
+ */
+
+public class Game {
+    String nom;
+
+    public Game(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
--- app/src/main/java/antoine/dechoudens/hesge/ch/ecalendar/presentation/MainActivity.java
+++ app/src/main/java/antoine/dechoudens/hesge/ch/ecalendar/presentation/MainActivity.java
@@ -5,30 +5,46 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import antoine.dechoudens.hesge.ch.ecalendar.R;
+import antoine.dechoudens.hesge.ch.ecalendar.base.GetFromUrl;
+import antoine.dechoudens.hesge.ch.ecalendar.base.NomsWebService;
+import antoine.dechoudens.hesge.ch.ecalendar.domain.Game;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GetFromUrl.Listener{
+    private List<Game> games;
+    private TextView tvTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        definirVariables();
+        initialise();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
+    private void definirVariables() {
+        tvTest = (TextView)findViewById(R.id.tvTest);
+    }
+
+    private void initialise() {
+        new GetFromUrl(this).execute(NomsWebService.URL_BASE);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,5 +66,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onGetFromUrlResult(JSONObject json) {
+        games = new ArrayList<>();
+        tvTest.setText("...Loading...");
+        try {
+            JSONArray JSONtags = json.getJSONArray("tag_list");
+            JSONArray JSONobs = json.getJSONArray("list");
+            tvTest.setText(JSONobs.get(0).toString());
+        } catch (JSONException e) {
+            tvTest.setText("Fuck");
+        }
+
+    }
+
+    @Override
+    public void onGetFromUrlError(Exception e) {
+        tvTest.setText(e.getMessage());
     }
 }
